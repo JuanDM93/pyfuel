3191f13211a8cf472fcd3b337728999d5e0a1748
JuanDM93 <juan_dm93@hotmail.com>
2016-02-29 13:53:55 -0500

    Works til:
    Characterizes
    Checks convergence
    First random image generation
    Can't improve pixel positions yet
    
    No GUI either... yet

---
+++ lil_funt.py
@@ -0,0 +1,36 @@
+import random
+
+
+def pix_txt(path):
+    with open(path) as f:
+        txt = f.read()
+    f.close()
+    return txt
+
+
+def read_Data(txt, start, end):
+    data = []
+    for i in range(start, end):
+        data.append(int(txt[i]))
+    return data
+
+
+def start_random(size):
+    values = []
+    for i in range(size):
+        values.append(random.randint(0, 1))
+    return values
+
+
+#       Check neighbours    ??Condition??
+def random_pix(old):
+    # for i in range(len(old)): old = old
+    old = start_random(len(old))
+    return old
+
+
+def writeOUT(data, path):
+    out = open(path, 'w')
+    for i in data:
+        out.write(i)
+    out.close()
